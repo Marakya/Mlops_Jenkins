6d0e11e905f91c5be6677e0f0e2769ba7a4179bc
Kate_Mayatskaya <113238801+Marakya@users.noreply.github.com>
2023-03-14 14:49:54 +0300

    Create requirements.py

---
+++ requirements.py
@@ -0,0 +1,8 @@
+import random
+import numpy as np
+import pandas as pd
+from sklearn.datasets import make_regression
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import pickle
+from sklearn.preprocessing import StandardScaler
